a9140efaf1a65c200f4f5332c0bb92b72531d769
Renan Galdino Truta <115110286+galdinorenantruta@users.noreply.github.com>
2023-07-24 18:10:58 -0300

    Fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./assets/Navbar";
 import Pricing from "./assets/Pricing";
+import Home from "./assets/Home";
 
 function App() {
   return (
@@ -9,14 +10,12 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/pricing" component={Pricing} />
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-const Home = () => <div>Main Page</div>;
 
 export default App;
